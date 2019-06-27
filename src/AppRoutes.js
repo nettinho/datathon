432541a84f69e3bdeb2b42cc0f1b875afb59f1c7
netto <euclydes.young@bluetab.net>
2019-06-27 11:29:13 +0200

    added rekognition

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -4,10 +4,12 @@ import React from 'react';
 import { Route } from "react-router-dom";
 
 import Test from './test_folder/Test'
+import Rekognition from './rekognition/Rekognition'
 
 const AppRoutes = () => (<>
   <Route exact path='/' component={() => <h1>Arcadia</h1>} />
   <Route exact path='/test' component={Test} />
+  <Route exact path='/rekognition' component={Rekognition} />
 </>);
 
 export default AppRoutes
